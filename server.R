8928ddf5be80e5e54c6523ff181ccd1b51e49898
Elsa Vera Amores <elsa.vera.amores@gmail.com>
2018-07-28 16:07:07 -0400

    Commented the code

--- server.R
+++ server.R
@@ -1,18 +1,9 @@
-#Function for PseudoPlot
-generate.pseudo.plot <- function( genes ) {
-
-  to_be_tested <- row.names(subset(fData(HSMM), gene_short_name %in% genes ))
-  cds_subset <- HSMM[to_be_tested,]
-  diff_test_res <- differentialGeneTest(cds_subset, fullModelFormulaStr = "~sm.ns(Pseudotime)")
-  diff_test_res[,c("gene_short_name", "pval", "qval")]
-  
-  plot_genes_in_pseudotime(cds_subset, color_by = "Clusters") + 
-    theme(legend.position = "top")
-}
 
 #Shiny Server
+
 shinyServer(function(input, output, session) {
   
+  # A reactive function to catch the errors due to unselected input in some of the render plots below
   catch_errors <- reactive({
     validate(
       need(input$gene != "", "Please select a gene"),
@@ -21,34 +12,51 @@ shinyServer(function(input, output, session) {
     
   })
   
+  # Interactive tSNE plot output showing the clustering analysis on the cells
   output$tSNEPlot <- renderPlot({
   TSNEPlot(object = pbmc, do.label=TRUE, no.axes=TRUE, pt.size = 0.2, label.size=4)
   })
   
   output$ClusterPlot <- renderPlot({
     catch_errors()
-    
+  
     FeaturePlot(object = pbmc, features.plot = input$gene, cols.use = c("grey", "blue"), reduction.use = "tsne", no.axes = TRUE)
   })
   
+  # Dot plot output showing the mean expression levels of each gene in each cluster
   output$dotPlot <- renderPlot({
     catch_errors()
     
     DotPlot(pbmc,genes.plot = input$gene, x.lab.rot = T, dot.scale = 8, plot.legend=T, col.min = 0)
   })
   
+  # Static plot output showing the developmental trajectory of expression changes from primitive stem cell to differentiated neuroblast
   output$pseudoPlot <- renderPlot({
     plot_cell_trajectory(HSMM, color_by = "Clusters") + theme(legend.position = "right") 
     
   })
   
+  # Interatcive plot output that shows the expression of specific genes in developmental pseudotime
   output$pseudoGenePlot <- renderPlot({
     catch_errors()
+    
+    #Function for PseudoPlot
+    generate.pseudo.plot <- function( genes ) {
+      
+      to_be_tested <- row.names(subset(fData(HSMM), gene_short_name %in% genes ))
+      cds_subset <- HSMM[to_be_tested,]
+      diff_test_res <- differentialGeneTest(cds_subset, fullModelFormulaStr = "~sm.ns(Pseudotime)")
+      diff_test_res[,c("gene_short_name", "pval", "qval")]
+      
+      plot_genes_in_pseudotime(cds_subset, color_by = "Clusters") + 
+        theme(legend.position = "top")
+    }
     
     generate.pseudo.plot(input$gene)
     
   })
   
+  # Interative heatmap that shows the expression levels of the top 15 (highly expresses) genes of each cluster cluster
   output$HeatMapPlot <- renderPlot({
     validate(
       need(input$focus.clusters != "", "Please select a cell type"),
